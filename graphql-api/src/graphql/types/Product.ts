077525cb46d50f44b736a4d094e4d0424b238ace
Nia RAMAROSANDY <ramarosandynia@gmail.com>
2020-11-20 11:22:14 +0100

    Add validation to the uuid field

--- graphql-api/src/graphql/types/Product.ts
+++ graphql-api/src/graphql/types/Product.ts
@@ -1,9 +1,11 @@
+import { IsUUID } from 'class-validator'
 import { Field, ID, InputType, ObjectType } from 'type-graphql'
 
 @InputType('ProductInput')
 @ObjectType()
 export default class Product {
     @Field((type) => ID)
+    @IsUUID()
     uuid!: string
     @Field()
     name!: string
